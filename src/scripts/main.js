d17a6042ee9b7c210c6c2bb16961d6456e441052
francesco.finotto <finoz.webdev@gmail.com>
2023-02-03 00:04:30 +0100

    canti - set feature - w.i.p.

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -5,15 +5,26 @@ import carouselUtils from "./utils/carouselUtils";
 
 class Setup {
   constructor() {
+    this.songbookUrl = "./songs/";
     this.log = "Start finoz/annaemich";
   }
+
+  listenForComponents() {
+    const compDispatcher = new ComponentDispatcher();
+    compDispatcher.createAsyncComponents().then(() => {
+      compDispatcher.observeDomChanges();
+    });
+  }
+
   init() {
     console.log(this.log);
     this.setCarousel();
+    this.setSongbook();
   }
 
   setCarousel() {
     let gallery = document.body.querySelector(".content-gallery");
+    if (!gallery) return;
     carouselUtils.setSwiperMarkup({
       $el: gallery,
       hasArrows: true,
@@ -33,6 +44,48 @@ class Setup {
       },
     });
   }
+
+  setSongbook() {
+    if (!window.songbook) return;
+    window.songbook.sort(function (a, b) {
+      return a.title.localeCompare(b.title);
+    });
+    window.songbook.forEach((song) => {
+      this.setSong(song);
+    });
+  }
+
+  async setSong(song) {
+    let url = this.songbookUrl + song.id + ".html";
+    const text = await this.getFetchedDom(url);
+    song.text = text;
+    let songMarkup = this.buildSongMarkup(song);
+    let songbook = document.body.querySelector(".page-content--canti");
+    songbook.insertAdjacentHTML("beforeend", songMarkup);
+  }
+
+  buildSongMarkup(song) {
+    let songMarkup = `
+			<details class="song" data-id="${song.id}" data-title="${song.title}" data-author="${song.author}">
+				<summary class="song-title">${song.title}</summary>
+				<div>`;
+    if (song.author) {
+      songMarkup += `<p class="song-author">${song.author}</p>`;
+    }
+    songMarkup += `<div class="song-text">${song.text}</div>
+		</div></details>
+		`;
+    return songMarkup;
+  }
+
+  // UTILS
+
+  async getFetchedDom(url) {
+    return fetch(url).then(function (response) {
+      return response.text();
+    });
+  }
 }
+
 let setup = new Setup();
 setup.init();
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,10 @@
 const path = require('path');
-const WebpackBar = require('webpackbar');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
-const SVGSpritemapPlugin = require('svg-spritemap-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const WebpackBar = require("webpackbar");
+const SVGSpritemapPlugin = require("svg-spritemap-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = (env, options) => {
-	return {
+  return {
     context: path.resolve(__dirname),
     devtool: "source-map",
     mode: options && options.mode ? options.mode : "development",
@@ -44,16 +43,6 @@ module.exports = (env, options) => {
           filename: "icons-sprite.svg",
         },
       }),
-      new BrowserSyncPlugin(
-        {
-          host: "localhost",
-          port: 3000,
-          proxy: "http://annaemich.local/",
-        },
-        {
-          reload: true,
-        }
-      ),
       new MiniCssExtractPlugin({
         filename: "main.css",
       }),
